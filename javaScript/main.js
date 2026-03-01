8424c0e3f7fe3f1e7044b854ac3b71862d15ace0
Hyan <hyanvic9@gmail.com>
2026-03-01 00:09:09 -0400

    implementa navegação e renderização de controles por domínio

--- javaScript/main.js
+++ javaScript/main.js
@@ -4,6 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
         carregarDominios();
     }
 
+    if (window.location.pathname.includes("controles.html")) {
+        carregarControles();
+    }
+
 });
 
 async function carregarDominios() {
@@ -22,10 +26,57 @@ async function carregarDominios() {
                 <p>${dominio.nome}</p>
             `;
 
+            div.style.cursor = "pointer";
+
+            div.addEventListener("click", () => {
+                localStorage.setItem("dominioSelecionado", dominio.numero);
+                window.location.href = "controles.html";
+            });
+
             container.appendChild(div);
         });
 
     } catch (erro) {
         console.error("Erro ao carregar JSON:", erro);
     }
+}
+
+async function carregarControles() {
+    try {
+        const resposta = await fetch("javaScript/data/iso27002.json");
+        const dados = await resposta.json();
+
+        const dominioNumero = localStorage.getItem("dominioSelecionado");
+
+        const dominio = dados.dominios.find(d => d.numero === dominioNumero);
+
+        const container = document.getElementById("lista-controles");
+
+        if (!dominio) {
+            container.innerHTML = "<p>Domínio não encontrado.</p>";
+            return;
+        }
+
+        dominio.objetivos.forEach(objetivo => {
+            objetivo.controles.forEach(controle => {
+
+                const div = document.createElement("div");
+                div.classList.add("card");
+
+                div.innerHTML = `
+                    <h3>${controle.codigo} - ${controle.titulo}</h3>
+                    <p>${controle.descricao}</p>
+                `;
+
+                container.appendChild(div);
+            });
+        });
+
+    } catch (erro) {
+        console.error("Erro ao carregar controles:", erro);
+    }
+
+    console.log("Dominio salvo:", dominioNumero);
+console.log("Dominios do JSON:", dados.dominios);
+console.log("Dominio encontrado:", dominio);
 }
